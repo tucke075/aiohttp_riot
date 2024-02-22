963f991d3bf756e79413b82ffba8540b5ec21ea3
tucke075 <tucke075@csusm.edu>
2024-02-21 16:23:19 -0800

    Created functions that parse json response accurately to display the placement of the game of the user intended with their augments.

--- bot/tft_data.py
+++ bot/tft_data.py
@@ -1,5 +1,5 @@
 import aiohttp
-import asyncio
+#import asyncio
 import creds
 
 
@@ -12,10 +12,8 @@ async def store_summoner_info(summoner_name):
         summoner_url = f'https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/{summoner_name}?api_key={creds.api_key}' 
         response = await session.get(summoner_url)
         summoner_info = await response.json()
-        recent_games = await get_recent_games(summoner_info)
-
-        #return summoner_info
-        return recent_games
+        worst_game = await get_recent_games(summoner_info)
+        return worst_game
     
 async def get_recent_games(summoner_info):
     games = []
@@ -24,7 +22,70 @@ async def get_recent_games(summoner_info):
         games_url = f'https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={creds.api_key}'
         response = await session.get(games_url)
         games = await response.json()
-        return games
+        bad_games = await store_bad_games(games,puuid)
+        formatted_game = await format_game(bad_games)
+        return formatted_game
+    
+async def store_bad_games(games,puuid):
+    bad_games = []
+    async with aiohttp.ClientSession() as session:
+        for game in games:
+            #make an api call for each game 
+            match_id = game
+            current_match_url = f'https://americas.api.riotgames.com/tft/match/v1/matches/{match_id}?api_key={creds.api_key}'
+            async with session.get(current_match_url) as response:
+                response = await session.get(current_match_url)
+                if response.status == 200:
+                    response = await response.json()
+                    game_info = response['info']
+                    for participant in game_info['participants']:
+                        if participant['puuid'] == puuid and participant['placement'] >= 5:
+                            
+                            important_info = {
+                                'match_id': match_id,
+                                'puuid' : puuid,
+                                'placement' : participant['placement'],
+                                'augments': participant.get('augments', []),
+                                'game_info': {
+                                    'endOfGameResult': game_info['endOfGameResult'],
+                                    'game_datetime': game_info['game_datetime'],
+                                    'game_length': game_info['game_length'],
+                                    'game_version': game_info['game_version'],
+                                }
+                                #'participant_info': participant
+                            }
+                            bad_games.append(important_info)
+        #should have list of all the placements >5 saved from recent 20 games
+        #check the worst of it
+        #any 8th will work for now
+        for bad_game in bad_games:
+            if puuid == bad_game['puuid']:
+                if bad_game['placement'] == 8:
+                    worst_game = bad_game
+                elif bad_game['placement'] == 7:
+                    worst_game = bad_game
+                elif bad_game['placement'] == 6:
+                    worst_game = bad_game
+                elif bad_game['placement'] == 5:
+                    worst_game = bad_game             
+    return worst_game
+
+
+async def format_game(game):
+    formatted_info = []
+    placement_val = str(game['placement'])
+    augments_val = ', '.join(game['augments'])
+    game_info = f"Placement: {placement_val}, your augments were {augments_val}"
+    formatted_info.append(game_info)
+    return formatted_info
+
+            
+
+
+
+            
+            
+
 
 
 #async def create_puuid_url(puuid):
